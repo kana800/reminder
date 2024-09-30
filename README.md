d64d8309c2e3b0d4253dd2262b317fc852b6d3ca
kanna <64830745+kana800@users.noreply.github.com>
2024-10-01 00:08:50 +0530

    added readme

--- README.md
+++ README.md
@@ -7,3 +7,31 @@ for example:
 2. remind `person-1` on their birthday;
 
 ### How It Works?
+
+1. Grab all the events for today from the `CalDav` server
+2. Arrange the data into format that is understood by `wa_bulk.py`
+```json
+{
+    "person1":["message 1","message 2"],
+    "person2":["message 1","message 2"]
+}
+```
+3. send the message to `wa_bulk.py`
+
+> above steps are carried out by [`sendreminder.ps1`](scripts/sendreminder.ps1)
+
+### Setting Up
+
+1. Install all the necessary modules
+```bash
+pip install -r requirements.txt
+```
+2. Create `env.py` file on the `src` and enter _login_ information of the `CalDav` Server
+```py
+CALURL = ""
+CALPASSWORD = ""
+CALUSERNAME = ""
+# path where the reminder-data is saved
+DATAPATH = ""
+```
+3. Run `main.py` to generate a `wa_bulk.py` compatible file
--- scripts/sendreminder.ps1
+++ scripts/sendreminder.ps1
@@ -1,14 +1,15 @@
-$pyWAPath = Resolve-Path -Path "G:\repositories\automate-whatsapp-messages\automater\wa_bulk.py"
-$pyReminderScriptPath = Resolve-Path -Path "G:\repositories\reminder\src\main.py"
-$pyWAExecPath = Resolve-Path -Path "G:\repositories\automate-whatsapp-messages\.venv\Scripts\python.exe"
-$pyReminderExecPath = Resolve-Path -Path "G:\repositories\reminder\.venv\Scripts\python.exe" 
+$pyWAPath = Resolve-Path -Path "g:\repositories\automate-whatsapp-messages\automater\wa_bulk.py"
+$pyReminderScriptPath = Resolve-Path -Path "g:\repositories\reminder\src\main.py"
+$pyWAExecPath = Resolve-Path -Path "g:\repositories\automate-whatsapp-messages\.venv\Scripts\python.exe"
+$pyReminderExecPath = Resolve-Path -Path "g:\repositories\reminder\.venv\Scripts\python.exe" 
 # creating a filename for wa_bulk.py 
 $currentDate = Get-Date -Format "dd-MM-yyyy"
-$fileDirectory = Resolve-Path -Path "G:\repositories\reminder\data"
+$fileDirectory = Resolve-Path -Path "g:\repositories\reminder\data\"
 $fileName = $fileDirectory.Path + $currentDate + "-ra.json"
 # running reminder script to generate content
 Write-Host "Fetching for reminders..."
-$pyRemOutput = & $pyReminderExecPath $pyReminderScriptPath
-Write-Host "Sending the messages..."
+#$pyRemOutput = & $pyReminderExecPath $pyReminderScriptPath
+#Write-Host "Sending the messages..."
+Write-Host $fileName
 $pyWAOutput = & $pyWAExecPath $pyWAPath $fileName
 Write-Host "Done"
